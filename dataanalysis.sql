9b41763780973e2099028e8fdc9f10ca0f962387
johnclos <81526953+johnclos@users.noreply.github.com>
2021-08-14 12:02:48 -0400

    Add files via upload
    updated dataanalysis.sql

--- dataanalysis.sql
+++ dataanalysis.sql
@@ -40,21 +40,27 @@ FROM dept_manager
 
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
--- : employee number, last name, first name, and department name.
 -- need to pull from employees: the employee number, last name, first name
 -- need to pull from departments: department number, department name
 -- need to pull from dept_emp: department number, employee number
 -- join departments and dept_emp on department number
 -- join employees and dept_emp on employee number
-
-
+SELECT dept_emp.emp_no,
+	departments.dept_name,
+	employees.last_name,
+	employees.first_name
+FROM dept_emp
+	FULL OUTER JOIN departments
+		ON departments.dept_no = dept_emp.dept_no
+	FULL OUTER JOIN employees ON
+		employees.emp_no = dept_emp.emp_no;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT employees.first_name,
 	employees.last_name,
 	employees.sex
 FROM employees
-WHERE first_name::text ilike 'hercules'
+WHERE first_name = 'Hercules'
 AND last_name::text like 'B%';
 
 
@@ -65,7 +71,16 @@ AND last_name::text like 'B%';
 -- join departments and dept_emp on department number
 -- join employees and dept_emp on employee number
 -- where employees in the Sales department
-
+SELECT dept_emp.emp_no,
+	departments.dept_name,
+	employees.last_name,
+	employees.first_name
+FROM dept_emp
+	FULL OUTER JOIN departments
+		ON departments.dept_no = dept_emp.dept_no
+	FULL OUTER JOIN employees ON
+		employees.emp_no = dept_emp.emp_no
+WHERE departments.dept_name = 'Sales';
 
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
@@ -75,7 +90,17 @@ AND last_name::text like 'B%';
 -- join departments and dept_emp on department number
 -- join employees and dept_emp on employee number
 -- where employees in the Sales and Development departments
-
+SELECT dept_emp.emp_no,
+	departments.dept_name,
+	employees.last_name,
+	employees.first_name
+FROM dept_emp
+	FULL OUTER JOIN departments
+		ON departments.dept_no = dept_emp.dept_no
+	FULL OUTER JOIN employees ON
+		employees.emp_no = dept_emp.emp_no;
+WHERE departments.dept_name = 'Sales'
+OR departments.dept_name = 'Development';
 
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
